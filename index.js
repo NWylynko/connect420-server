9f8317073094396050ccff7fea5c214332fb9aff
Nick Wylynko <nick1014375@gmail.com>
2020-04-11 18:02:55 +0800

    how the flip flop did i do that

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Game } = require("./Game");
 const app = require('express')();
 const cors = require('cors');
 app.use(cors("connect420.web.app"));
-const http = require('http').Server(app);
+const helmet = require('helmet')
 app.use(helmet())
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
