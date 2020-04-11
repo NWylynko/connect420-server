753b9f3c34d048a6ae28c9b1c7ed69aee734639c
Nick Wylynko <nick1014375@gmail.com>
2020-04-11 18:00:36 +0800

    add helmet for security

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = require('express')();
 const cors = require('cors');
 app.use(cors("connect420.web.app"));
 const http = require('http').Server(app);
+app.use(helmet())
+const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = 3001;
 
