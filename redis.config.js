4021db1d89b87d08d1323417cae0c6bb30a52e33
Nick Wylynko <nick1014375@gmail.com>
2020-04-12 18:49:02 +0800

    add example redis config

---
+++ redis.config.js
@@ -0,0 +1,13 @@
+// settings for redis connection
+// options at https://redis.js.org/#-api-rediscreateclient
+
+module.exports = {
+  host: "127.0.0.1",
+  port: 6379,
+  path: null,
+  url: null,
+  auth_pass: null,
+  db: null,
+  family: 'IPv4',
+  prefix: null,
+}
