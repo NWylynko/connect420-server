c9ec41f812a0df8721b8038732b92c47dfc9636c
Nick Wylynko <nick1014375@gmail.com>
2020-04-15 15:28:10 +0800

    add reference

--- redis.config.js
+++ redis.config.js
@@ -1,5 +1,5 @@
 // settings for redis connection
-// options at https://redis.js.org/#-api-rediscreateclient
+// options at https://redis.js.org/#-api-rediscreateclient and https://www.npmjs.com/package/redis#options-object-properties
 
 module.exports = {
   host: "127.0.0.1",
